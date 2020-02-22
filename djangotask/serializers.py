546c08c25f3dc5e4b231a5a39907836e6a43a1ea
surajt <smartsurajit2008@gmail.com>
2020-02-22 14:38:30 +0300

    minor edits

--- djangotask/serializers.py
+++ djangotask/serializers.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class ClientRequestDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = ClientRequestDetail
-        fields = ('ip', 'browser_detail','content_type','query_string')
+        fields = ('ip', 'browser_detail','content_type','query_string',)
