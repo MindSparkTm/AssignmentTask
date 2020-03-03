6bab695e8f38f3bd32f0b27fd83ff4a11f4986c8
surajt <smartsurajit2008@gmail.com>
2020-03-04 01:00:33 +0300

    removed unnecessary files

--- djangotask/serializers.py
+++
@@ -1,7 +0,0 @@
-from .models import ClientRequestDetail
-from rest_framework import serializers
-
-class ClientRequestDetailSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ClientRequestDetail
-        fields = ('ip', 'browser_detail','content_type','query_string',)
